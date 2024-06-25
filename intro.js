c3a738f81cbc51777a43d91c44568533d088f4fb
iconrizwankhan <rizwankhan.1992@gmail.com>
2024-06-25 10:02:47 +0530

    ist commit on this repo

---
+++ intro.js
@@ -0,0 +1,87 @@
+// /* Types of Data type = string, boolean, number (integers: Javascript doesnt have integer), Float, object, nAN
+// boolean = true and false
+// variables = var, const, let
+// String literals =``
+// concat
+// ===  comparison operater
+// equals to operater read more about
+// */
+// var f = "info"; // = is an assigin and here we are declaring and initilizating
+// console.log(f);
+// console.error(f);
+// console.warn(f);
+// console.count(f);
+// console.info(f);
+
+// // testing Assignment operatorss
+// const w = 10;
+// var z = (w+2)/w;
+// var y = 1+z;
+// console.log("when w=", w, "then z=", z, "and y=", y);
+// console.log(w, z, y);
+
+// // h=2;
+// // b= ( h += 3);
+// // console.log(b);
+
+// // testing Arithmetic operatorss
+// y = 5;
+// ++y;
+// console.log(++y);
+
+// y = 5;
+// let x=++y;
+// console.log("x=",x);
+
+// // a=5;
+// // a++;
+// // console.log(a++);
+
+// // a= 5;
+// // let b = a++;
+// // console.log("b=",b);
+
+// i=8;
+// j=i--;
+// console.log(j);
+
+// i=8;
+// j= --i;
+// console.log(j);
+
+// m = 10
+// m % 3;
+// console.log(m % 3);
+
+// n = 9;
+// n / 3;
+// console.log(n/3);
+
+// // testing Comparison operatorss
+// a = 1;
+// b = "1";
+// console.log(a !== b);
+
+// c = 3 / 4;
+// d = 5 / 4;
+// console.log(c, d);
+// console.log(d >= c);
+// console.log(d <= c);
+
+// n=53;
+
+// const a = 3*n + 2*n; 
+
+// console.log(a);
+// console.log("hello" === "hello");
+// console.log("1" === 1);
+// console.log(0 === false);
+
+// var a =10, b =15, c = 12.5;
+// a==++a;
+// console.log(a==++a);
+
+// logical operaters
+//&& : and operator prints first flase statement/value
+//|| : or operator prints first true statement/value
+console.log("" | 0);
